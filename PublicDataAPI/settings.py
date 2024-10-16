633ae1c026e3345c816d8e933c58ddc0f092aa0f
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-10-16 19:42:51 +0100

    Create Docker config

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -171,3 +171,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-10-16T18:00:00
 # auto-update 2024-10-16T18:34:17
 # auto-update 2024-10-16T19:08:34
+# auto-update 2024-10-16T19:42:51
