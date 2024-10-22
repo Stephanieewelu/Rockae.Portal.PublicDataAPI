73d24aa650f58ff880644e13d197889016355fc7
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-10-22 19:12:00 +0100

    Remove quiz model

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -188,3 +188,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-10-22T18:00:00
 # auto-update 2024-10-22T18:24:00
 # auto-update 2024-10-22T18:48:00
+# auto-update 2024-10-22T19:12:00
