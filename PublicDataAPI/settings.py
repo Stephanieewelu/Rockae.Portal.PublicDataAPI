7abadcbdc74d48eae2fdf8d3ccc236d64dcc4ed4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-01 19:05:27 +0000

    Enhance CORS policy

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -211,3 +211,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-01T18:00:00
 # auto-update 2024-11-01T18:21:49
 # auto-update 2024-11-01T18:43:38
+# auto-update 2024-11-01T19:05:27
