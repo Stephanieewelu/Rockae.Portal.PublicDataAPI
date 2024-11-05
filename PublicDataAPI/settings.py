59a9bc7a2238f42f39dfc7421779dcf13568c0a3
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-05 19:49:05 +0000

    Add pagination

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -224,3 +224,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-05T18:43:38
 # auto-update 2024-11-05T19:05:27
 # auto-update 2024-11-05T19:27:16
+# auto-update 2024-11-05T19:49:05
