87cb11ba4becc83ec66427058601292a74c286a8
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-05 20:32:43 +0000

    Configure category model

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -226,3 +226,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-05T19:27:16
 # auto-update 2024-11-05T19:49:05
 # auto-update 2024-11-05T20:10:54
+# auto-update 2024-11-05T20:32:43
