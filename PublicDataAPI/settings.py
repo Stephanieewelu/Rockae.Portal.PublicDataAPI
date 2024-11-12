eb2694fded818ee83add8e948561ad16065a9a75
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-12 21:00:00 +0000

    Refactor submission handler

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -255,3 +255,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-12T20:00:00
 # auto-update 2024-11-12T20:20:00
 # auto-update 2024-11-12T20:40:00
+# auto-update 2024-11-12T21:00:00
