a18a96d3ef6087e20b544f6d093e8dccabdcd2e9
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-13 21:25:42 +0000

    Rename category model

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -264,3 +264,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-13T19:42:51
 # auto-update 2024-11-13T20:17:08
 # auto-update 2024-11-13T20:51:25
+# auto-update 2024-11-13T21:25:42
