15b36021e99f939f1f91281a148438f50f37f8ec
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-20 19:59:56 +0000

    Optimize dashboard stats

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -282,3 +282,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-20T19:08:32
 # auto-update 2024-11-20T19:25:40
 # auto-update 2024-11-20T19:42:48
+# auto-update 2024-11-20T19:59:56
