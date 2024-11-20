c41dc695b3dd0d8f2cb3eef0c68b9bd9374a6ee2
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-20 18:51:24 +0000

    Test project structure

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -278,3 +278,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-20T18:00:00
 # auto-update 2024-11-20T18:17:08
 # auto-update 2024-11-20T18:34:16
+# auto-update 2024-11-20T18:51:24
