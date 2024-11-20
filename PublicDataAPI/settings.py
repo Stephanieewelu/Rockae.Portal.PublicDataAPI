bf645c09ed4aaac4060188b588f9f5ccc8db0016
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-20 18:17:08 +0000

    Optimize favicon

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -276,3 +276,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-14T21:12:00
 # auto-update 2024-11-14T21:36:00
 # auto-update 2024-11-20T18:00:00
+# auto-update 2024-11-20T18:17:08
