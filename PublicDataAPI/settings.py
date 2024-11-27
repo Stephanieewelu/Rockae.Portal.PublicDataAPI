b7c12f1e9144f3ca70ca4847fda387f9fb5b3abb
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-11-27 19:42:48 +0000

    Secure email templates

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -300,3 +300,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-11-27T18:51:24
 # auto-update 2024-11-27T19:08:32
 # auto-update 2024-11-27T19:25:40
+# auto-update 2024-11-27T19:42:48
