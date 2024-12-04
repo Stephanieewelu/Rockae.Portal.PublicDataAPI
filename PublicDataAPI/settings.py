889e80214b76d946062aa0e7cf5516266126331d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-04 20:17:08 +0000

    Update serializer

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -336,3 +336,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-04T18:34:17
 # auto-update 2024-12-04T19:08:34
 # auto-update 2024-12-04T19:42:51
+# auto-update 2024-12-04T20:17:08
