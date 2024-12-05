a02938d73bd2b586ca76a9c1097b4991be44000d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-05 20:51:25 +0000

    Optimize Docker config

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -344,3 +344,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-05T19:08:34
 # auto-update 2024-12-05T19:42:51
 # auto-update 2024-12-05T20:17:08
+# auto-update 2024-12-05T20:51:25
