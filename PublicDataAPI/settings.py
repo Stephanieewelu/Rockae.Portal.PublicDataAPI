15a0934c71607bcd36868f7089b20628e030217e
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-05 19:08:34 +0000

    Style admin panel

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -341,3 +341,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-04T21:25:42
 # auto-update 2024-12-05T18:00:00
 # auto-update 2024-12-05T18:34:17
+# auto-update 2024-12-05T19:08:34
