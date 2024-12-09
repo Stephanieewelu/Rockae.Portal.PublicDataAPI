73fee197bb0fc0315b54868c1169e56b0d4da7d3
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-09 19:50:42 +0000

    Document mobile layout

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -352,3 +352,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-09T18:55:21
 # auto-update 2024-12-09T19:13:48
 # auto-update 2024-12-09T19:32:15
+# auto-update 2024-12-09T19:50:42
