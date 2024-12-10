8f93a50eff32d53d36e7ac05067a5178309bdd4c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-10 18:24:00 +0000

    Rename submission handler

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -360,3 +360,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-09T21:22:57
 # auto-update 2024-12-09T21:41:24
 # auto-update 2024-12-10T18:00:00
+# auto-update 2024-12-10T18:24:00
