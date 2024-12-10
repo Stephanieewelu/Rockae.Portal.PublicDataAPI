67db196ac664d5e3503364d9cc310d10fd614e4e
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-10 20:48:00 +0000

    Implement quiz analytics

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -366,3 +366,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-10T19:36:00
 # auto-update 2024-12-10T20:00:00
 # auto-update 2024-12-10T20:24:00
+# auto-update 2024-12-10T20:48:00
