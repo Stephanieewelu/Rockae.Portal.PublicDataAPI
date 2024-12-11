709f0be2d6938e37a57952b9b9db41acc8187946
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-11 20:40:00 +0000

    Enhance search bar

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -375,3 +375,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-11T19:20:00
 # auto-update 2024-12-11T19:46:40
 # auto-update 2024-12-11T20:13:20
+# auto-update 2024-12-11T20:40:00
