3490174dec792652f2f2604bf8869085ed5a3081
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-11 18:53:20 +0000

    Remove quiz validation

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -371,3 +371,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-10T21:36:00
 # auto-update 2024-12-11T18:00:00
 # auto-update 2024-12-11T18:26:40
+# auto-update 2024-12-11T18:53:20
