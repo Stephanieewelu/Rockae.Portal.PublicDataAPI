b578133f2bb29309341046ec92e358ba98389bcd
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-12 20:48:00 +0000

    Rename utility functions

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -385,3 +385,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-12T19:36:00
 # auto-update 2024-12-12T20:00:00
 # auto-update 2024-12-12T20:24:00
+# auto-update 2024-12-12T20:48:00
