949aa291b69e6432247a5c42c00b107d2099db58
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-12 18:48:00 +0000

    Style question randomizer

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -380,3 +380,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-11T21:33:20
 # auto-update 2024-12-12T18:00:00
 # auto-update 2024-12-12T18:24:00
+# auto-update 2024-12-12T18:48:00
