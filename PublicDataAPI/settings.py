a651709ae2ae562e9b07dd1d1ca572d446f903df
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-12 20:00:00 +0000

    Optimize deployment script

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -383,3 +383,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-12T18:48:00
 # auto-update 2024-12-12T19:12:00
 # auto-update 2024-12-12T19:36:00
+# auto-update 2024-12-12T20:00:00
