b453d2f57f3bd1616979a02efe6128a469b4b1fb
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-13 18:26:40 +0000

    Secure notification system

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -389,3 +389,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-12T21:12:00
 # auto-update 2024-12-12T21:36:00
 # auto-update 2024-12-13T18:00:00
+# auto-update 2024-12-13T18:26:40
