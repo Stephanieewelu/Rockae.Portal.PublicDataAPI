d0eaad3d6112a74bd792c174e27012e985b570f4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-20 21:00:00 +0000

    Refactor question randomizer

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -416,3 +416,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-20T19:30:00
 # auto-update 2024-12-20T20:00:00
 # auto-update 2024-12-20T20:30:00
+# auto-update 2024-12-20T21:00:00
