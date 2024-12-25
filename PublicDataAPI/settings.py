4dbdc4e5b9b6fe177238e9a216e0b22c9b9913bf
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-25 19:08:32 +0000

    Style typos in comments

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -432,3 +432,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-25T18:17:08
 # auto-update 2024-12-25T18:34:16
 # auto-update 2024-12-25T18:51:24
+# auto-update 2024-12-25T19:08:32
