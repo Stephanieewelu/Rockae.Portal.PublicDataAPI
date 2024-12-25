b13d5189b447be99fc93e9483f349cb19bdb7255
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-25 18:17:08 +0000

    Update unit tests

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -429,3 +429,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-23T21:12:00
 # auto-update 2024-12-23T21:36:00
 # auto-update 2024-12-25T18:00:00
+# auto-update 2024-12-25T18:17:08
