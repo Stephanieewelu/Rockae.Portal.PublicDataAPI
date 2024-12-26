42b740a813bfd69efeb56be18758c39327d52fc2
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-26 18:00:00 +0000

    Validate deployment script

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -442,3 +442,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-25T21:08:28
 # auto-update 2024-12-25T21:25:36
 # auto-update 2024-12-25T21:42:44
+# auto-update 2024-12-26T18:00:00
