68ed9f77a773d652e1db2eba7d16359bf9d67167
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-26 19:40:00 +0000

    Set up submission handler

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -447,3 +447,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-26T18:40:00
 # auto-update 2024-12-26T19:00:00
 # auto-update 2024-12-26T19:20:00
+# auto-update 2024-12-26T19:40:00
