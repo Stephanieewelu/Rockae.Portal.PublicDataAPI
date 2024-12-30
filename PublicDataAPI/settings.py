ed86b137db664facd1fa8f2ed74b136729e00edb
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-12-30 18:40:00 +0000

    Validate database indexes

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -460,3 +460,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2024-12-29T16:12:00
 # auto-update 2024-12-29T18:36:00
 # auto-update 2024-12-30T18:00:00
+# auto-update 2024-12-30T18:40:00
