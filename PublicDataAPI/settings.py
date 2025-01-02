4fe812ebf4531f2b18afa77c0b1528c9d8724005
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-02 21:00:00 +0000

    Style feedback form

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -471,3 +471,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-02T19:30:00
 # auto-update 2025-01-02T20:00:00
 # auto-update 2025-01-02T20:30:00
+# auto-update 2025-01-02T21:00:00
