e755f48347a36dec4029d1ad2e3f3781c42ed9a3
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-10 18:53:20 +0000

    Optimize quiz model

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -475,3 +475,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-02T21:30:00
 # auto-update 2025-01-10T18:00:00
 # auto-update 2025-01-10T18:26:40
+# auto-update 2025-01-10T18:53:20
