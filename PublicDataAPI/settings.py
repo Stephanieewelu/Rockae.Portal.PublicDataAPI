945a294f6dd92f81c03c14c71594e4266fd3b449
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-12 19:00:00 +0000

    Fix session timeout

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -505,3 +505,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-12T16:00:00
 # auto-update 2025-01-12T17:00:00
 # auto-update 2025-01-12T18:00:00
+# auto-update 2025-01-12T19:00:00
