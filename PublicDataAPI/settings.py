7a7b767d55a90c063437e11a7577e47b20396715
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-14 21:41:24 +0000

    Rename test coverage

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -524,3 +524,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-14T20:46:03
 # auto-update 2025-01-14T21:04:30
 # auto-update 2025-01-14T21:22:57
+# auto-update 2025-01-14T21:41:24
