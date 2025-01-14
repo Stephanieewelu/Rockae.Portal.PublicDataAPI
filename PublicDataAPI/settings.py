8119e25df2b64b1929c9a4c2212e226efbeae78e
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-14 20:46:03 +0000

    Fix quiz filters

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -521,3 +521,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-14T19:50:42
 # auto-update 2025-01-14T20:09:09
 # auto-update 2025-01-14T20:27:36
+# auto-update 2025-01-14T20:46:03
