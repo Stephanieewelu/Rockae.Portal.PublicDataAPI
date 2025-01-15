f9efdcee906db0f44e4092cc6241d8e24c274b4f
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-15 21:00:00 +0000

    Enhance quiz attempts

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -534,3 +534,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-15T20:00:00
 # auto-update 2025-01-15T20:20:00
 # auto-update 2025-01-15T20:40:00
+# auto-update 2025-01-15T21:00:00
