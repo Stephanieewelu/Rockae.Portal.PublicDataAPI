2be9446a183563c20e705e43187658351e11b6ca
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-19 13:48:00 +0000

    Configure access control

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -544,3 +544,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-16T21:12:00
 # auto-update 2025-01-19T09:00:00
 # auto-update 2025-01-19T11:24:00
+# auto-update 2025-01-19T13:48:00
