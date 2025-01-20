b2ad793a53a76820cc3d10467ec2ac714cce99f1
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-20 20:00:00 +0000

    Rename quiz model

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -552,3 +552,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-20T18:48:00
 # auto-update 2025-01-20T19:12:00
 # auto-update 2025-01-20T19:36:00
+# auto-update 2025-01-20T20:00:00
