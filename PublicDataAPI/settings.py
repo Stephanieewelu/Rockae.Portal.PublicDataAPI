b2abd471534a9c31e4ce7686a8069777f710acb0
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-30 19:42:48 +0000

    Initialize dark mode

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -593,3 +593,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-30T18:51:24
 # auto-update 2025-01-30T19:08:32
 # auto-update 2025-01-30T19:25:40
+# auto-update 2025-01-30T19:42:48
