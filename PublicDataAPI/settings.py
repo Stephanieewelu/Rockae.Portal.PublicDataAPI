02e1f6b64c27059db8294eb210d648be3a09f71e
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-01-30 19:08:32 +0000

    Set up user auth

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -591,3 +591,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-01-30T18:17:08
 # auto-update 2025-01-30T18:34:16
 # auto-update 2025-01-30T18:51:24
+# auto-update 2025-01-30T19:08:32
