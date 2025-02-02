cba62bc4ac1d52c0ffd8f267c40b5e3e3728deb4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-02-02 11:46:09 +0000

    Document serializer

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -609,3 +609,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-02-02T09:00:00
 # auto-update 2025-02-02T09:55:23
 # auto-update 2025-02-02T10:50:46
+# auto-update 2025-02-02T11:46:09
