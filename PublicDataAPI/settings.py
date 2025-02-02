bb9f28bf85c9f2cedebdf12a0fe67e9f2d6621e8
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-02-02 10:50:46 +0000

    Rename typos in comments

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -608,3 +608,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-02-01T18:36:00
 # auto-update 2025-02-02T09:00:00
 # auto-update 2025-02-02T09:55:23
+# auto-update 2025-02-02T10:50:46
