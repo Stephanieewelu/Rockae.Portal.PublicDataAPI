4116a7b5e029869b8bf581e5baa47ba11fc1fe04
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-02-12 21:25:42 +0000

    Configure test coverage

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -680,3 +680,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-02-12T19:42:51
 # auto-update 2025-02-12T20:17:08
 # auto-update 2025-02-12T20:51:25
+# auto-update 2025-02-12T21:25:42
