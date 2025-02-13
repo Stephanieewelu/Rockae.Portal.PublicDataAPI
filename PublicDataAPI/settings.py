00d8e34eae2b2d5ef9c5eb5b6dafc799e5d1fd7c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-02-13 20:00:00 +0000

    Validate score calculator

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -687,3 +687,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-02-13T19:00:00
 # auto-update 2025-02-13T19:20:00
 # auto-update 2025-02-13T19:40:00
+# auto-update 2025-02-13T20:00:00
