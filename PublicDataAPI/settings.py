894754d15362e7dea19c1f6d4ebabe765fe798fe
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-02-17 19:59:56 +0000

    Update README

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -700,3 +700,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-02-17T19:08:32
 # auto-update 2025-02-17T19:25:40
 # auto-update 2025-02-17T19:42:48
+# auto-update 2025-02-17T19:59:56
