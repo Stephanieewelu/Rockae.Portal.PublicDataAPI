8e06398fe5fd1a013864e6cad0474be860d5df50
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-02-21 20:24:00 +0000

    Enhance loading spinner

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -713,3 +713,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-02-21T19:12:00
 # auto-update 2025-02-21T19:36:00
 # auto-update 2025-02-21T20:00:00
+# auto-update 2025-02-21T20:24:00
