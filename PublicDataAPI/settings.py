4049f2e96ed1c8abb962e555df71553ff6f9dd65
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-02-27 20:17:04 +0000

    Update CI/CD config

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -770,3 +770,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-02-27T19:25:40
 # auto-update 2025-02-27T19:42:48
 # auto-update 2025-02-27T19:59:56
+# auto-update 2025-02-27T20:17:04
