87c1644682550c037139aa197bf11a7e07b3d00b
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-06 18:34:16 +0000

    Optimize CSS bugs

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -804,3 +804,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-03T21:42:44
 # auto-update 2025-03-06T18:00:00
 # auto-update 2025-03-06T18:17:08
+# auto-update 2025-03-06T18:34:16
