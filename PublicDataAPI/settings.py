68174a0aa89a35828551138652b3403df92388f1
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-07 20:54:32 +0000

    Rename quiz retry feature

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -824,3 +824,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-07T19:49:05
 # auto-update 2025-03-07T20:10:54
 # auto-update 2025-03-07T20:32:43
+# auto-update 2025-03-07T20:54:32
