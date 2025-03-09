a8a77431d56bacc24d219e63fb662f0a8427a192
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-09 20:04:36 +0000

    Improve quiz retry feature

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -852,3 +852,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-09T17:18:27
 # auto-update 2025-03-09T18:13:50
 # auto-update 2025-03-09T19:09:13
+# auto-update 2025-03-09T20:04:36
