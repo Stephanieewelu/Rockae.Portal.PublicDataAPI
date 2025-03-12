0a07fbffaaeb2e3dfdb414ade0bbd7ac4b16d9b9
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-12 21:41:24 +0000

    Enhance console warnings

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -873,3 +873,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-12T20:46:03
 # auto-update 2025-03-12T21:04:30
 # auto-update 2025-03-12T21:22:57
+# auto-update 2025-03-12T21:41:24
