bda56191195776e86560f01faa89b39a46e5262c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-12 18:55:21 +0000

    Remove notification system

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -864,3 +864,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-12T18:00:00
 # auto-update 2025-03-12T18:18:27
 # auto-update 2025-03-12T18:36:54
+# auto-update 2025-03-12T18:55:21
