fa5574ce5ef1ebbdfa781f30f73fbfa57d4b03c8
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-12 20:09:09 +0000

    Integrate Docker config

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -868,3 +868,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-12T19:13:48
 # auto-update 2025-03-12T19:32:15
 # auto-update 2025-03-12T19:50:42
+# auto-update 2025-03-12T20:09:09
