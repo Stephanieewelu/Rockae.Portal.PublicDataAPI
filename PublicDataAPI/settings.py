21b2fe67bcfab779c7eb2ddbbb97975f43cd32dd
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-14 18:34:17 +0000

    Configure CSS bugs

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -875,3 +875,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-12T21:22:57
 # auto-update 2025-03-12T21:41:24
 # auto-update 2025-03-14T18:00:00
+# auto-update 2025-03-14T18:34:17
