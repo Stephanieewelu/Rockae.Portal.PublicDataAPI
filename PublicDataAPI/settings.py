b6822561e440fead9c478a45a1a6bc2d78e104e4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-18 21:38:10 +0000

    Style quiz analytics

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -891,3 +891,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-18T20:32:43
 # auto-update 2025-03-18T20:54:32
 # auto-update 2025-03-18T21:16:21
+# auto-update 2025-03-18T21:38:10
