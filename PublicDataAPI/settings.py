eba55902ddbf41a318d9fb9d47700721f8644e66
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-03-29 12:36:00 +0000

    Document response time

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -917,3 +917,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-29T09:00:00
 # auto-update 2025-03-29T10:12:00
 # auto-update 2025-03-29T11:24:00
+# auto-update 2025-03-29T12:36:00
