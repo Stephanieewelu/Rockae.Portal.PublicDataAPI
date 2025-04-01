1993a1056943349bdca0345ddc64c1cb377691ec
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-01 18:48:00 +0100

    Validate quiz results page

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -926,3 +926,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-03-29T19:48:00
 # auto-update 2025-04-01T18:00:00
 # auto-update 2025-04-01T18:24:00
+# auto-update 2025-04-01T18:48:00
