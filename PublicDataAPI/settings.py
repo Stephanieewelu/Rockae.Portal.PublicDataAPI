92cb8ab779d2dd5adfc890670a8386d7641a252c
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-12 11:10:54 +0100

    Secure database indexes

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -962,3 +962,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-10T21:40:00
 # auto-update 2025-04-12T09:00:00
 # auto-update 2025-04-12T10:05:27
+# auto-update 2025-04-12T11:10:54
