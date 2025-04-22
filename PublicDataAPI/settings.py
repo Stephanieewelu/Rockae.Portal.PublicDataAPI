ccc01304eae6ebf874a1fc25f02c4be495f7b387
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-22 21:00:00 +0100

    Document payment integration

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -996,3 +996,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-22T19:30:00
 # auto-update 2025-04-22T20:00:00
 # auto-update 2025-04-22T20:30:00
+# auto-update 2025-04-22T21:00:00
