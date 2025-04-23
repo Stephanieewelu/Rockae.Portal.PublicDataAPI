e5f4a60dc56c4c8bfefd59fe73c8749d289211c5
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-23 19:05:27 +0100

    Test typos in comments

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1001,3 +1001,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-23T18:00:00
 # auto-update 2025-04-23T18:21:49
 # auto-update 2025-04-23T18:43:38
+# auto-update 2025-04-23T19:05:27
