7469f902675948beb24bca8db4bc55bf4769c6a5
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-24 21:06:40 +0100

    Rename score calculator

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1016,3 +1016,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-24T19:46:40
 # auto-update 2025-04-24T20:13:20
 # auto-update 2025-04-24T20:40:00
+# auto-update 2025-04-24T21:06:40
