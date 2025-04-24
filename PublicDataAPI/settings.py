38a27c446848957d2a24449ab09f35f20e235295
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-24 18:00:00 +0100

    Optimize rate limiter

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1009,3 +1009,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-23T20:54:32
 # auto-update 2025-04-23T21:16:21
 # auto-update 2025-04-23T21:38:10
+# auto-update 2025-04-24T18:00:00
