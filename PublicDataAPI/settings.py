362f6b41e4534c19c3050a94538d63f68aea44f6
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-25 19:32:15 +0100

    Configure quiz attempts

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1023,3 +1023,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-25T18:36:54
 # auto-update 2025-04-25T18:55:21
 # auto-update 2025-04-25T19:13:48
+# auto-update 2025-04-25T19:32:15
