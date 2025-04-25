08b0660d39771c7374bf12e391ab951ced2e4d96
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-25 21:41:24 +0100

    Refactor console warnings

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1030,3 +1030,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-25T20:46:03
 # auto-update 2025-04-25T21:04:30
 # auto-update 2025-04-25T21:22:57
+# auto-update 2025-04-25T21:41:24
