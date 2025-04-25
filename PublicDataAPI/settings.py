636a5395a19dd02635ade7c4f1c06a0f0e61fe14
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-25 21:04:30 +0100

    Implement error handling

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1028,3 +1028,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-25T20:09:09
 # auto-update 2025-04-25T20:27:36
 # auto-update 2025-04-25T20:46:03
+# auto-update 2025-04-25T21:04:30
