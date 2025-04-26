2f27569c1d3e0c3bbdf91dd444fea4b3edcc3124
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-26 18:00:00 +0100

    Rename favicon

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1037,3 +1037,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-26T13:30:00
 # auto-update 2025-04-26T15:00:00
 # auto-update 2025-04-26T16:30:00
+# auto-update 2025-04-26T18:00:00
