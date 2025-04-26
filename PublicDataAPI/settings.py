0e923a833dd1becea92c2b851a40c9b674c41a43
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-04-26 13:30:00 +0100

    Refactor unit tests

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1034,3 +1034,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-04-26T09:00:00
 # auto-update 2025-04-26T10:30:00
 # auto-update 2025-04-26T12:00:00
+# auto-update 2025-04-26T13:30:00
