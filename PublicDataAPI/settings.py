a1d1a205320e7527565a539dfa5d7d0fb923ba61
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-05-01 19:27:16 +0100

    Secure utility functions

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1063,3 +1063,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-05-01T18:21:49
 # auto-update 2025-05-01T18:43:38
 # auto-update 2025-05-01T19:05:27
+# auto-update 2025-05-01T19:27:16
