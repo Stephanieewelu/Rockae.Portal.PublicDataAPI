2e328587303146fd190095a70f37ec71ac08d501
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-05-05 20:27:36 +0100

    Rename database indexes

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1085,3 +1085,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-05-05T19:32:15
 # auto-update 2025-05-05T19:50:42
 # auto-update 2025-05-05T20:09:09
+# auto-update 2025-05-05T20:27:36
