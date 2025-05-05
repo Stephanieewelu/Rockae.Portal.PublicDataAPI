1a8eee1141df115e3583820e2918d8a5f5b13c46
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-05-05 21:41:24 +0100

    Secure markdown support

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1089,3 +1089,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-05-05T20:46:03
 # auto-update 2025-05-05T21:04:30
 # auto-update 2025-05-05T21:22:57
+# auto-update 2025-05-05T21:41:24
