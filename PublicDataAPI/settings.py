28a271da419cd358cc7cf139bb962d952d9b5464
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-05-06 18:26:40 +0100

    Fix views

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1091,3 +1091,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-05-05T21:22:57
 # auto-update 2025-05-05T21:41:24
 # auto-update 2025-05-06T18:00:00
+# auto-update 2025-05-06T18:26:40
