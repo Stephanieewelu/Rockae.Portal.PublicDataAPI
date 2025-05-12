f3f5c957e07a40a10283215e21dadb7aa4b0a64f
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-05-12 19:12:00 +0100

    Refactor CORS policy

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1115,3 +1115,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-05-12T18:00:00
 # auto-update 2025-05-12T18:24:00
 # auto-update 2025-05-12T18:48:00
+# auto-update 2025-05-12T19:12:00
