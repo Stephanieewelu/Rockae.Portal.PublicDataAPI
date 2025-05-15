ac118c4a4c63ed3fd552956d174d3b88991077b6
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2025-05-15 20:51:25 +0100

    Create API endpoints

--- PublicDataAPI/settings.py
+++ PublicDataAPI/settings.py
@@ -1127,3 +1127,4 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 # auto-update 2025-05-15T19:08:34
 # auto-update 2025-05-15T19:42:51
 # auto-update 2025-05-15T20:17:08
+# auto-update 2025-05-15T20:51:25
